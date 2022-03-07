1152cef024198e38a8cfb62a7a447c8e1609f2a5
Christy Seddon <89155206+CSeddon01@users.noreply.github.com>
2022-03-06 20:05:37 -0600

    fixed pathing issue on server.js

--- server.js
+++ server.js
@@ -1,36 +1,26 @@
-
 //calling dependencies. intstalled by using: npm i express
-const express = require('express');
+const express = require("express");
 const app = express();
-const fs = require('fs');
-const path = require('path');
-const uniqueId = require('uniqid');
+const fs = require("fs");
+const path = require("path");
+const uniqueId = require("uniqid");
 //Built in Middleware that is used
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(express.static('/public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-
-
-app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/notes.html'));
-});
 //Routes used
-console.log(__dirname);
-app.get('/api/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, './db/db.json'));
+
+app.get("/api/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./db/db.json"));
 });
 
-
-
-
-
-app.post('/api/notes' , (req, res) => {
+app.post("/api/notes", (req, res) => {
   const newNote = req.body;
-  const notes = JSON.parse(fs.readFileSync('./db/db.json'));
+  const notes = JSON.parse(fs.readFileSync("./db/db.json"));
   newNote.id = uniqueId();
   notes.push(newNote);
-  fs.writeFileSync('./db/db.json', JSON.stringify(notes));
+  fs.writeFileSync("./db/db.json", JSON.stringify(notes));
   res.json(notes);
 });
 
@@ -42,14 +32,15 @@ app.delete("/api/notes/:id", (req, res) => {
   res.json(noteList);
 });
 
-app.get('*' , (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/index.html'));
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
 
 //starts the local server on port 3001
 app.listen(process.env.PORT || 3001, () => {
-    console.log('Server Running in Port 3001')
-  });
-  
-  
+  console.log("Server Running in Port 3001");
+});
